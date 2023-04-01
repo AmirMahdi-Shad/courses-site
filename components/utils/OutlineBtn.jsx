1f9d877509dcf1e3649992a1507e3c9fec515757
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2023-04-02 00:02:00 +0430

    change text color of header

--- components/utils/OutlineBtn.jsx
+++ components/utils/OutlineBtn.jsx
@@ -1,7 +1,7 @@
 const OutlineBtn = ({ text }) => {
   return (
     <>
-      <button className='px-7 py-3 border-2 border-white rounded-full '>
+      <button className='px-7 py-3 border-2 border-white text-white rounded-full '>
         {text}
       </button>
     </>
